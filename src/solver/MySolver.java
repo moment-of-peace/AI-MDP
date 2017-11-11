d58d7ae70fe12490e4bcb2d38372d9677ffadab9
moment-of-peace <yi.liu1183@gmail.com>
2017-11-11 14:16:56 +1000

    value iteration frame 2

--- src/solver/MySolver.java
+++ src/solver/MySolver.java
@@ -16,83 +16,97 @@ import problem.Matrix;
 import problem.ProblemSpec;
 
 public class MySolver implements FundingAllocationAgent {
-	
-	private ProblemSpec spec = new ProblemSpec();
-	private VentureManager ventureManager;
+
+    private ProblemSpec spec = new ProblemSpec();
+    private VentureManager ventureManager;
     private List<Matrix> probabilities;
     private HashMap<FundState, Integer[]> policy;
-    private HashMap<FundState, Double> values;
-	
-	public MySolver(ProblemSpec spec) throws IOException {
-	    this.spec = spec;
-		ventureManager = spec.getVentureManager();
+    private HashMap<FundState, Double> rewards;
+
+    public MySolver(ProblemSpec spec) throws IOException {
+        this.spec = spec;
+        ventureManager = spec.getVentureManager();
         probabilities = spec.getProbabilities();
         policy = new HashMap<FundState, Integer[]>();
-        values = new HashMap<FundState, Double>();
-	}
-	
-	public void doOfflineComputation() {
-	    // initially, V(s) = R(s), i.e. compute the immediate best rewards for all states
-	    HashMap<FundState, Double> previousValues = rewards();
-	    while (true) {
-	        valueIteration();
-	        previousValues = values;
-	    }
-	}
+        rewards = new HashMap<FundState, Double>();
+    }
 
-    public List<Integer> generateAdditionalFundingAmounts(List<Integer> manufacturingFunds,
-														  int numFortnightsLeft) {
-		// Example code that allocates an additional $10 000 to each venture.
-		// TODO Replace this with your own code.
+    public void doOfflineComputation() {
+        // initially, V(s) = R(s), i.e. compute the immediate best rewards for all states
+        rewards = getRewards();
+        HashMap<FundState, Double> previousValues = rewards;
+        HashMap<FundState, Double> currentValues = new HashMap<FundState, Double>();
+        ArrayList<Double[][]> transfer = getTransMatrix();
+        do {
+            valueIteration(previousValues, currentValues, transfer);
+            previousValues = currentValues;
+        } while (!converge(previousValues, currentValues));
+    }
 
-		List<Integer> additionalFunding = new ArrayList<Integer>();
+    public List<Integer> generateAdditionalFundingAmounts(List<Integer> manufacturingFunds, int numFortnightsLeft) {
+        // Example code that allocates an additional $10 000 to each venture.
+        // TODO Replace this with your own code.
 
-		int totalManufacturingFunds = 0;
-		for (int i : manufacturingFunds) {
-			totalManufacturingFunds += i;
-		}
-		
-		int totalAdditional = 0;
-		for (int i = 0; i < ventureManager.getNumVentures(); i++) {
-			if (totalManufacturingFunds >= ventureManager.getMaxManufacturingFunds() ||
-			        totalAdditional >= ventureManager.getMaxAdditionalFunding()) {
-				additionalFunding.add(0);
-			} else {
-				additionalFunding.add(1);
-				totalAdditional ++;
-				totalManufacturingFunds ++;
-			}
-		}
+        List<Integer> additionalFunding = new ArrayList<Integer>();
 
-		return additionalFunding;
-	}
-    
-    private HashMap<FundState, Double> rewards() {
+        int totalManufacturingFunds = 0;
+        for (int i : manufacturingFunds) {
+            totalManufacturingFunds += i;
+        }
+
+        int totalAdditional = 0;
+        for (int i = 0; i < ventureManager.getNumVentures(); i++) {
+            if (totalManufacturingFunds >= ventureManager.getMaxManufacturingFunds()
+                    || totalAdditional >= ventureManager.getMaxAdditionalFunding()) {
+                additionalFunding.add(0);
+            } else {
+                additionalFunding.add(1);
+                totalAdditional++;
+                totalManufacturingFunds++;
+            }
+        }
+
+        return additionalFunding;
+    }
+
+    private ArrayList<Double[][]> getTransMatrix() {
         // TODO Auto-generated method stub
         return null;
     }
     
-    private void valueIteration() {
+    // immediate max reward of each fund state 
+    private HashMap<FundState, Double> getRewards() {
         // TODO Auto-generated method stub
-        
+        return null;
+    }
+
+    private void valueIteration(HashMap<FundState, Double> previous, HashMap<FundState, Double> current, 
+            ArrayList<Double[][]> transfer) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    // return true if the |values - previousValues| is small enough
+    private boolean converge(HashMap<FundState, Double> previous, HashMap<FundState, Double> current) {
+        return false;
     }
 }
 
 class FundState {
     protected Integer[] states;
-    
+
     public FundState(Integer[] funds) {
         states = new Integer[funds.length];
         for (int i = 0; i < funds.length; i++) {
             states[i] = funds[i];
         }
     }
-    
+
     @Override
     public int hashCode() {
         int h = 0;
         for (int i = 0; i < states.length; i++) {
-            h = 31*h + states[i];
+            h = 31 * h + states[i];
         }
         return h;
     }
