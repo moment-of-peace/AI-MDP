f71506f3381cffb72751217e9b0bd8479a9a375f
moment-of-peace <yi.liu1183@gmail.com>
2017-11-11 13:32:03 +1000

    value iteration frame

--- src/solver/MySolver.java
+++ src/solver/MySolver.java
@@ -8,6 +8,7 @@ package solver;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import problem.VentureManager;
@@ -19,18 +20,27 @@ public class MySolver implements FundingAllocationAgent {
 	private ProblemSpec spec = new ProblemSpec();
 	private VentureManager ventureManager;
     private List<Matrix> probabilities;
+    private HashMap<FundState, Integer[]> policy;
+    private HashMap<FundState, Double> values;
 	
 	public MySolver(ProblemSpec spec) throws IOException {
 	    this.spec = spec;
 		ventureManager = spec.getVentureManager();
         probabilities = spec.getProbabilities();
+        policy = new HashMap<FundState, Integer[]>();
+        values = new HashMap<FundState, Double>();
 	}
 	
 	public void doOfflineComputation() {
-	    // TODO Write your own code here.
+	    // initially, V(s) = R(s), i.e. compute the immediate best rewards for all states
+	    HashMap<FundState, Double> previousValues = rewards();
+	    while (true) {
+	        valueIteration();
+	        previousValues = values;
+	    }
 	}
-	
-	public List<Integer> generateAdditionalFundingAmounts(List<Integer> manufacturingFunds,
+
+    public List<Integer> generateAdditionalFundingAmounts(List<Integer> manufacturingFunds,
 														  int numFortnightsLeft) {
 		// Example code that allocates an additional $10 000 to each venture.
 		// TODO Replace this with your own code.
@@ -56,5 +66,34 @@ public class MySolver implements FundingAllocationAgent {
 
 		return additionalFunding;
 	}
+    
+    private HashMap<FundState, Double> rewards() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    private void valueIteration() {
+        // TODO Auto-generated method stub
+        
+    }
+}
 
+class FundState {
+    protected Integer[] states;
+    
+    public FundState(Integer[] funds) {
+        states = new Integer[funds.length];
+        for (int i = 0; i < funds.length; i++) {
+            states[i] = funds[i];
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        int h = 0;
+        for (int i = 0; i < states.length; i++) {
+            h = 31*h + states[i];
+        }
+        return h;
+    }
 }
