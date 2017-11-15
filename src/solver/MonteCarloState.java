a5cb7540e1525c76c8d3792eb9d07812879a15e9
Yi Liu (刘奕） <yi.liu1183@gmail.com>
2017-11-15 17:26:09 +1000

    modify monte carlo state

--- src/solver/MonteCarloState.java
+++ src/solver/MonteCarloState.java
@@ -6,6 +6,7 @@ package solver;
  */
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MonteCarloState {
     protected List<Integer> fundState;
@@ -16,41 +17,31 @@ public class MonteCarloState {
         this.fundState = new ArrayList<Integer>(manufacturingFunds);
     }
 
+    // return all possible next state based on current state
     public List<MonteCarloState> allPossibleStates(int maxFund, int maxAdd) {
-        List<Integer> actionLimit = new ArrayList<Integer>();
-        for (int i = 0; i < this.fundState.size(); i++) {
-            actionLimit.add(maxFund-this.fundState.get(i));
-        }
-        List<List<Integer>> allActions = findAllActions(actionLimit, maxAdd, actionLimit.size());
+        //List<Integer> actionLimit = getActionLimit(maxFund);
+        maxAdd = getMaxAdd(maxFund, maxAdd);
+        List<List<Integer>> allActions = findAllActions(maxAdd, this.fundState.size());
         List<MonteCarloState> nextStates = new ArrayList<MonteCarloState>();
+        // apply all possible actions to current fund state
         for (List<Integer> action: allActions) {
             nextStates.add(applyAction(action));
         }
         return nextStates;
     }
-
-    private MonteCarloState applyAction(List<Integer> action) {
-        List<Integer> newState = new ArrayList<Integer>();
-        for (int i = 0; i < action.size(); i++) {
-            newState.add(this.fundState.get(i) + action.get(i));
-        }
-        return new MonteCarloState(newState);
-    }
-
-    private List<List<Integer>> findAllActions(List<Integer> actionLimit, int maxAdd, int size) {
+    // find all valid actions can be applied to the current fund state
+    private List<List<Integer>> findAllActions(int maxAdd, int size) {
         List<List<Integer>> allActions = new ArrayList<List<Integer>>();
-        int index = size - 1;   // ?
         if (size == 1) {
-            for (int i = 0; i < Math.min(actionLimit.get(index), maxAdd); i++) {
+            for (int i = 0; i < maxAdd; i++) {
                 List<Integer> action = new ArrayList<Integer>();
                 action.add(i);
                 allActions.add(action);
-                //maxAdd--;
             }
         }
         else {
-            for (int i = 0; i < Math.min(actionLimit.get(i), maxAdd); i++) {
-                for (List<Integer> action: findAllActions(actionLimit, maxAdd-i, size-1)) {
+            for (int i = 0; i < maxAdd; i++) {
+                for (List<Integer> action: findAllActions(maxAdd-i, size-1)) {
                     List<Integer> action2 = new ArrayList<Integer>(action); // necessary?
                     action2.add(i);
                     allActions.add(action2);
@@ -60,19 +51,47 @@ public class MonteCarloState {
         return allActions;
     }
     
+    // return a random next state based on current state
     public MonteCarloState randNextState(int maxFund, int maxAdd) {
-        List<Integer> actionLimit = new ArrayList<Integer>();
-        for (int i = 0; i < this.fundState.size(); i++) {
-            actionLimit.add(maxFund-this.fundState.get(i));
+        //List<Integer> actionLimit = getActionLimit(maxFund);
+        maxAdd = getMaxAdd(maxFund, maxAdd);
+        List<Integer> action = findRandNext(maxAdd, this.fundState.size());
+        return applyAction(action);
+    }
+    // randomly find a valid action which can be applied to current fund state
+    private List<Integer> findRandNext(int maxAdd, int size) {
+        int limit = maxAdd + 1;
+        Random rand = new Random();
+        List<Integer> action;
+        if (size == 1) {
+            action = new ArrayList<Integer>();
+            action.add(rand.nextInt(limit));
         }
-        
-        maxAdd = getMaxAdd(maxAdd);
-        return null;
+        else {
+            int fund = rand.nextInt(limit);
+            action = new ArrayList<Integer>(findRandNext(maxAdd-fund, size-1)); // necessary?
+            action.add(fund);
+        }
+        return action;
     }
 
-    private int getMaxAdd(int maxAdd) {
-        // TODO Auto-generated method stub
-        return 0;
+    // based on the max funding and max additional funding, return the limit of action
+    private int getMaxAdd(int maxFund, int maxAdd) {
+        int sum = 0;
+        for (int i: this.fundState) {
+            sum += i;
+        }
+        int rest = maxFund - sum;
+        return rest<maxAdd ? rest:maxAdd;
+    }
+    
+    // return the result after applying an action to current fund state
+    private MonteCarloState applyAction(List<Integer> action) {
+        List<Integer> newState = new ArrayList<Integer>();
+        for (int i = 0; i < action.size(); i++) {
+            newState.add(this.fundState.get(i) + action.get(i));
+        }
+        return new MonteCarloState(newState);
     }
 
 }
